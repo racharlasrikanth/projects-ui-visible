7067bb5ab1a8a9c4f1238881f6458393ac26c465
Srikanth <sri1@publicisgroupe.net>
2021-11-22 14:13:49 +0530

    javascript functionality added

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const projectContainer = document.querySelectorAll(".projects-container");
+const projectContainer = document.querySelector(".projects-container");
 console.log(projectContainer);
 
-const URL = "https://javascript-projects-api-data.netlify.app/javascriptData.json"
+// const URL = "https://javascript-projects-api-data.netlify.app/javascriptData.json";
+const URL = "./data.json";
+let javascriptApiData = [];
 
 const jsData = async () => {
-    const response = await fetch(URL, { headers: { accept: "Accept: application/json" } })
-      console.log(response);
+    const response = await fetch(URL)
     const myJson = await response.json();
-    console.log(myJson);
+    javascriptApiData = myJson.results;
+
+    javascriptApiData.forEach(item => {
+      const htmlText = `
+      <article class="project" id="${item.id}">
+        <a class="main-link" target="_blank" href="${item.url}"></a>
+        <div class="img-container">
+            <img src="${item.thumbnail}" alt="${item.projectName}">
+        </div>
+        <div class="project-info">
+            <h4 class="project-title">${item.projectName}</h4>
+            <div class="technologies">
+                <span class="tech">html</span>
+                <span class="tech">css</span>
+                <span class="tech">javascript</span>
+            </div>
+            <footer class="project-footer">
+                <p tabindex="0" class="project-view">view</p>
+                <p tabindex="0" class="project-sourcecode">code</p>
+            </footer>
+        </div>
+      </article>
+      `
+      projectContainer.insertAdjacentHTML("beforeend", htmlText);
+    });
 } 
-    
 jsData();
